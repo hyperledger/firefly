410245a88416841681b54e588b2a3f609cb9d032
Gabriel Indik <gabriel.indik@kaleido.io>
2020-11-11 20:59:18 -0500

    Payment instance creation tests
    Signed-off-by: Gabriel Indik <gabriel.indik@kaleido.io>

--- core/src/test/payments/authored.ts
+++ core/src/test/payments/authored.ts
@@ -2,16 +2,15 @@ import { app, mockEventStreamWebSocket } from '../common';
 import nock from 'nock';
 import request from 'supertest';
 import assert from 'assert';
-import { IDBPaymentDefinition, IEventPaymentDefinitionCreated } from '../../lib/interfaces';
+import { IDBPaymentDefinition, IDBPaymentInstance, IEventPaymentDefinitionCreated, IEventPaymentInstanceCreated } from '../../lib/interfaces';
 import * as utils from '../../lib/utils';
 
 describe('Payment definitions: authored', async () => {
 
   let paymentDefinitionID: string;
+  const timestamp = utils.getTimestamp();
 
   describe('Create payment definition', () => {
-
-    const timestamp = utils.getTimestamp();
 
     it('Checks that the payment definition can be added', async () => {
 
@@ -79,6 +78,92 @@ describe('Payment definitions: authored', async () => {
         .get(`/api/v1/payments/definitions/${paymentDefinitionID}`)
         .expect(200);
       assert.deepStrictEqual(paymentDefinition, getPaymentDefinitionResponse.body);
+    });
+
+  });
+
+  describe('Payment instances', async () => {
+
+    let paymentInstanceID: string;
+
+    it('Checks that a payment instance can be created', async () => {
+
+      nock('https://apigateway.kaleido.io')
+        .post('/createPaymentInstance?kld-from=0x0000000000000000000000000000000000000001&kld-sync=false')
+        .reply(200);
+
+      const result = await request(app)
+        .post('/api/v1/payments/instances')
+        .send({
+          paymentDefinitionID,
+          author: '0x0000000000000000000000000000000000000001',
+          recipient: '0x0000000000000000000000000000000000000002',
+          amount: 10
+        })
+        .expect(200);
+      assert.deepStrictEqual(result.body.status, 'submitted');
+      paymentInstanceID = result.body.paymentInstanceID;
+
+      const getPaymentInstancesResponse = await request(app)
+        .get('/api/v1/payments/instances')
+        .expect(200);
+      const paymentInstance = getPaymentInstancesResponse.body.find((paymentInstance: IDBPaymentInstance) => paymentInstance.paymentInstanceID === paymentInstanceID);
+      assert.strictEqual(paymentInstance.author, '0x0000000000000000000000000000000000000001');
+      assert.strictEqual(paymentInstance.paymentDefinitionID, paymentDefinitionID);
+      assert.strictEqual(paymentInstance.recipient, '0x0000000000000000000000000000000000000002');
+      assert.strictEqual(paymentInstance.amount, 10);
+      assert.strictEqual(paymentInstance.confirmed, false);
+      assert.strictEqual(typeof paymentInstance.timestamp, 'number');
+
+      const getPaymentInstanceResponse = await request(app)
+        .get(`/api/v1/payments/instances/${paymentInstanceID}`)
+        .expect(200);
+      assert.deepStrictEqual(paymentInstance, getPaymentInstanceResponse.body);
+
+    });
+
+    it('Checks that the event stream notification for confirming the payment instance creation is handled', async () => {
+      const eventPromise = new Promise((resolve) => {
+        mockEventStreamWebSocket.once('send', message => {
+          assert.strictEqual(message, '{"type":"ack","topic":"dev"}');
+          resolve();
+        })
+      });
+      const data: IEventPaymentInstanceCreated = {
+        paymentDefinitionID: utils.uuidToHex(paymentDefinitionID),
+        author: '0x0000000000000000000000000000000000000001',
+        paymentInstanceID: utils.uuidToHex(paymentInstanceID),
+        amount: '10',
+        recipient: '0x0000000000000000000000000000000000000002',
+        timestamp: timestamp.toString()
+      };
+      mockEventStreamWebSocket.emit('message', JSON.stringify([{
+        signature: utils.contractEventSignatures.PAYMENT_INSTANCE_CREATED,
+        data,
+        blockNumber: '123',
+        transactionHash: '0x0000000000000000000000000000000000000000000000000000000000000000'
+      }]));
+      await eventPromise;
+    });
+
+    it('Checks that the payment instance is confirmed', async () => {
+      const getAssetInstancesResponse = await request(app)
+        .get('/api/v1/payments/instances')
+        .expect(200);
+      const paymentInstance = getAssetInstancesResponse.body.find((paymentInstance: IDBPaymentInstance) => paymentInstance.paymentInstanceID === paymentInstanceID);
+      assert.strictEqual(paymentInstance.author, '0x0000000000000000000000000000000000000001');
+      assert.strictEqual(paymentInstance.recipient, '0x0000000000000000000000000000000000000002');
+      assert.strictEqual(paymentInstance.paymentDefinitionID, paymentDefinitionID);
+      assert.strictEqual(paymentInstance.confirmed, true);
+      assert.strictEqual(paymentInstance.amount, 10);
+      assert.strictEqual(paymentInstance.timestamp, timestamp);
+      assert.strictEqual(paymentInstance.blockchainData.blockNumber, 123);
+      assert.strictEqual(paymentInstance.blockchainData.transactionHash, '0x0000000000000000000000000000000000000000000000000000000000000000');
+
+      const getAssetInstanceResponse = await request(app)
+        .get(`/api/v1/payments/instances/${paymentInstanceID}`)
+        .expect(200);
+      assert.deepStrictEqual(paymentInstance, getAssetInstanceResponse.body);
     });
 
   });
--- core/src/test/payments/unauthored.ts
+++ core/src/test/payments/unauthored.ts
@@ -1,16 +1,16 @@
 import { app, mockEventStreamWebSocket } from '../common';
 import request from 'supertest';
 import assert from 'assert';
-import { IEventPaymentDefinitionCreated, IDBPaymentDefinition } from '../../lib/interfaces';
+import { IEventPaymentDefinitionCreated, IDBPaymentDefinition, IEventPaymentInstanceCreated, IDBPaymentInstance } from '../../lib/interfaces';
 import * as utils from '../../lib/utils';
+import nock from 'nock';
 
 describe('Payment definitions: unauthored', async () => {
 
   const paymentDefinitionID = 'f9812952-50f8-4090-9412-e7b0f3eeb930';
+  const timestamp = utils.getTimestamp();
 
   describe('Payment definition', async () => {
-
-    const timestamp = utils.getTimestamp();
 
     it('Checks that the event stream notification for confirming the payment definition creation is handled', async () => {
 
@@ -52,6 +52,93 @@ describe('Payment definitions: unauthored', async () => {
       .get(`/api/v1/payments/definitions/${paymentDefinitionID}`)
       .expect(200);
       assert.deepStrictEqual(paymentDefinition, getPaymentDefinitionResponse.body);
+    });
+
+  });
+
+
+  describe('Payment instances', async () => {
+
+    let paymentInstanceID: string;
+
+    it('Checks that a payment instance can be created', async () => {
+
+      nock('https://apigateway.kaleido.io')
+        .post('/createPaymentInstance?kld-from=0x0000000000000000000000000000000000000001&kld-sync=false')
+        .reply(200);
+
+      const result = await request(app)
+        .post('/api/v1/payments/instances')
+        .send({
+          paymentDefinitionID,
+          author: '0x0000000000000000000000000000000000000001',
+          recipient: '0x0000000000000000000000000000000000000002',
+          amount: 10
+        })
+        .expect(200);
+      assert.deepStrictEqual(result.body.status, 'submitted');
+      paymentInstanceID = result.body.paymentInstanceID;
+
+      const getPaymentInstancesResponse = await request(app)
+        .get('/api/v1/payments/instances')
+        .expect(200);
+      const paymentInstance = getPaymentInstancesResponse.body.find((paymentInstance: IDBPaymentInstance) => paymentInstance.paymentInstanceID === paymentInstanceID);
+      assert.strictEqual(paymentInstance.author, '0x0000000000000000000000000000000000000001');
+      assert.strictEqual(paymentInstance.paymentDefinitionID, paymentDefinitionID);
+      assert.strictEqual(paymentInstance.recipient, '0x0000000000000000000000000000000000000002');
+      assert.strictEqual(paymentInstance.amount, 10);
+      assert.strictEqual(paymentInstance.confirmed, false);
+      assert.strictEqual(typeof paymentInstance.timestamp, 'number');
+
+      const getPaymentInstanceResponse = await request(app)
+        .get(`/api/v1/payments/instances/${paymentInstanceID}`)
+        .expect(200);
+      assert.deepStrictEqual(paymentInstance, getPaymentInstanceResponse.body);
+
+    });
+
+    it('Checks that the event stream notification for confirming the payment instance creation is handled', async () => {
+      const eventPromise = new Promise((resolve) => {
+        mockEventStreamWebSocket.once('send', message => {
+          assert.strictEqual(message, '{"type":"ack","topic":"dev"}');
+          resolve();
+        })
+      });
+      const data: IEventPaymentInstanceCreated = {
+        paymentDefinitionID: utils.uuidToHex(paymentDefinitionID),
+        author: '0x0000000000000000000000000000000000000001',
+        paymentInstanceID: utils.uuidToHex(paymentInstanceID),
+        amount: '10',
+        recipient: '0x0000000000000000000000000000000000000002',
+        timestamp: timestamp.toString()
+      };
+      mockEventStreamWebSocket.emit('message', JSON.stringify([{
+        signature: utils.contractEventSignatures.PAYMENT_INSTANCE_CREATED,
+        data,
+        blockNumber: '123',
+        transactionHash: '0x0000000000000000000000000000000000000000000000000000000000000000'
+      }]));
+      await eventPromise;
+    });
+
+    it('Checks that the payment instance is confirmed', async () => {
+      const getAssetInstancesResponse = await request(app)
+        .get('/api/v1/payments/instances')
+        .expect(200);
+      const paymentInstance = getAssetInstancesResponse.body.find((paymentInstance: IDBPaymentInstance) => paymentInstance.paymentInstanceID === paymentInstanceID);
+      assert.strictEqual(paymentInstance.author, '0x0000000000000000000000000000000000000001');
+      assert.strictEqual(paymentInstance.recipient, '0x0000000000000000000000000000000000000002');
+      assert.strictEqual(paymentInstance.paymentDefinitionID, paymentDefinitionID);
+      assert.strictEqual(paymentInstance.confirmed, true);
+      assert.strictEqual(paymentInstance.amount, 10);
+      assert.strictEqual(paymentInstance.timestamp, timestamp);
+      assert.strictEqual(paymentInstance.blockchainData.blockNumber, 123);
+      assert.strictEqual(paymentInstance.blockchainData.transactionHash, '0x0000000000000000000000000000000000000000000000000000000000000000');
+
+      const getAssetInstanceResponse = await request(app)
+        .get(`/api/v1/payments/instances/${paymentInstanceID}`)
+        .expect(200);
+      assert.deepStrictEqual(paymentInstance, getAssetInstanceResponse.body);
     });
 
   });
