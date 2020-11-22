6cbd171c185d7df16653dc0516095f4ca9468f61
Gabriel Indik <gabriel.indik@kaleido.io>
2020-11-22 11:01:53 -0500

    App2App based client event handling

---
+++ core/src/handlers/client-events.ts
@@ -0,0 +1,15 @@
+import { ClientEventType } from "../lib/interfaces";
+import { config } from '../lib/config';
+import { settings } from '../lib/settings';
+import * as utils from '../lib/utils';
+import * as app2app from '../clients/app2app';
+import { createLogger, LogLevelString } from 'bunyan';
+
+const log = createLogger({ name: 'handlers/client-events.ts', level: utils.constants.LOG_LEVEL as LogLevelString });
+
+export const clientEventHandler = (eventType: ClientEventType, content: object) => {
+  if (settings.clientEvents.includes(eventType)) {
+    log.trace(`Dispatched client event ${eventType}`);
+    app2app.dispatchMessage(config.app2app.destinations.client, JSON.stringify({ type: eventType, content }));
+  }
+};
--- core/src/lib/settings.ts
+++ core/src/lib/settings.ts
@@ -1,14 +1,19 @@
+import Ajv from 'ajv';
 import { promisify } from 'util';
 import { readFile, writeFile } from 'fs';
 import path from 'path';
 import * as utils from './utils';
 import { createLogger, LogLevelString } from 'bunyan';
+import { ISettings } from './interfaces';
+import settingsSchema from '../schemas/settings.json';
 
 const log = createLogger({ name: 'lib/settings.ts', level: utils.constants.LOG_LEVEL as LogLevelString });
 
 const asyncReadFile = promisify(readFile);
 const asyncWriteFile = promisify(writeFile);
-export let values: {[key: string]: any};
+const ajv = new Ajv();
+const validateSettings = ajv.compile(settingsSchema);
+export let settings: ISettings;
 
 const settingsFilePath = path.join(utils.constants.DATA_DIRECTORY, utils.constants.SETTINGS_FILE_NAME);
 
@@ -18,24 +23,30 @@ export const init = async () => {
 
 const readSettingsFile = async () => {
   try {
-    values = JSON.parse(await asyncReadFile(settingsFilePath, 'utf8'));
+    const values = JSON.parse(await asyncReadFile(settingsFilePath, 'utf8'));
+    if(!validateSettings(values)) {
+      throw new Error('Invalid content');
+    }
+    settings = values;
   } catch(err) {
     if(err.errno === -2) {
-      values = {};
+      settings = {
+        clientEvents: []
+      };
     } else {
-      throw new Error(`Failed to read configuration file. ${err}`);
+      throw new Error(`Failed to read settings file. ${err}`);
     }
   }
 };
 
-export const setSetting = (key: string, value: any) => {
-  values[key] = value;
+export const setClientEvents = (clientEvents: string[]) => {
+  settings.clientEvents = clientEvents;
   persistSettings();
 };
 
 const persistSettings = () => {
   try {
-    asyncWriteFile(settingsFilePath, JSON.stringify(values));
+    asyncWriteFile(settingsFilePath, JSON.stringify(settings));
   } catch(err) {
     log.error(`Failed to persist settings. ${err}`);
   }
