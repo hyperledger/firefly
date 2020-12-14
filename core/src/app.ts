b7e1902f3abee3d8e8198d952183ae2b145c1892
Peter Broadhurst <peter.broadhurst@kaleido.io>
2020-12-13 23:18:11 -0500

    Move error catch to app, not index

--- core/src/app.ts
+++ core/src/app.ts
@@ -1,3 +1,9 @@
+import { createLogger, LogLevelString } from 'bunyan';
 import { start } from './index';
+import * as utils from './lib/utils';
 
-export const promise = start();
+const log = createLogger({ name: 'index.ts', level: utils.constants.LOG_LEVEL as LogLevelString });
+
+export const promise = start().catch(err => {
+  log.error(`Failed to start asset trail. ${err}`);
+});
