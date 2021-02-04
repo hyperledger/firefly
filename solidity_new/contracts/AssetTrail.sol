61e00b40d1f36f17c4538251be7f662b20fa4bda
Alex Shorsher <alex.shorsher@kaleido.io>
2021-02-04 11:18:49 -0500

    solidity contract changes
    Condensing asset definition creation into one function/event

--- solidity_new/contracts/AssetTrail.sol
+++ solidity_new/contracts/AssetTrail.sol
@@ -13,44 +13,8 @@ contract AssetTrail {
         uint timestamp
     );
 
-    event DescribedStructuredAssetDefinitionCreated (
-        bytes32 assetDefinitionID,
-        address author,
-        string name,
-        bool isContentPrivate,
-        bool isContentUnique,
-        bytes32 descriptionSchemaHash,
-        bytes32 contentSchemaHash,
-        uint timestamp
-    );
-    
-    event DescribedUnstructuredAssetDefinitionCreated (
-        bytes32 assetDefinitionID,
-        address author,
-        string name,
-        bool isContentPrivate,
-        bool isContentUnique,
-        bytes32 descriptionSchemaHash,
-        uint timestamp
-    );
-    
-    event StructuredAssetDefinitionCreated (
-        bytes32 assetDefinitionID,
-        address author,
-        string name,
-        bool isContentPrivate,
-        bool isContentUnique,
-        bytes32 contentSchemaHash,
-        uint timestamp
-    );
-
-    event UnstructuredAssetDefinitionCreated (
-        bytes32 assetDefinitionID,
-        address author,
-        string name,
-        bool isContentPrivate,
-        bool isContentUnique,
-        uint timestamp
+    event AssetDefinitionCreated (
+        bytes32 assetDefinitionHash
     );
 
     event DescribedAssetInstanceCreated (
@@ -75,7 +39,7 @@ contract AssetTrail {
         address author,
         uint timestamp
     );
-    
+
     event DescribedPaymentDefinitionCreated (
         bytes32 paymentDefinitionID,
         address author,
@@ -100,7 +64,7 @@ contract AssetTrail {
         bytes32 descriptionHash,
         uint timestamp
     );
-    
+
     event PaymentInstanceCreated (
         bytes32 paymentInstanceID,
         bytes32 paymentDefinitionID,
@@ -124,30 +88,14 @@ contract AssetTrail {
     constructor(address paymentContract) public {
         payment = ERC20(paymentContract);
     }
-    
+
     function registerMember(string memory name, string memory assetTrailInstanceID, string memory app2appDestination, string memory docExchangeDestination) public {
         require(bytes(name).length != 0, "Invalid name");
         emit MemberRegistered(msg.sender, name, assetTrailInstanceID, app2appDestination, docExchangeDestination, now);
     }
-    
-    function createDescribedStructuredAssetDefinition(bytes32 assetDefinitionID, string memory name, bool isContentPrivate, bool isContentUnique, bytes32 descriptionSchemaHash, bytes32 contentSchemaHash) public {
-        require(bytes(name).length != 0, "Invalid name");
-        emit DescribedStructuredAssetDefinitionCreated(assetDefinitionID, msg.sender, name, isContentPrivate, isContentUnique, descriptionSchemaHash, contentSchemaHash, now);
-    }
 
-    function createDescribedUnstructuredAssetDefinition(bytes32 assetDefinitionID, string memory name, bool isContentPrivate, bool isContentUnique, bytes32 descriptionSchemaHash) public {
-        require(bytes(name).length != 0, "Invalid name");
-        emit DescribedUnstructuredAssetDefinitionCreated(assetDefinitionID, msg.sender, name, isContentPrivate, isContentUnique, descriptionSchemaHash, now);
-    }
-    
-    function createStructuredAssetDefinition(bytes32 assetDefinitionID, string memory name, bool isContentPrivate, bool isContentUnique, bytes32 contentSchemaHash) public {
-        require(bytes(name).length != 0, "Invalid name");
-        emit StructuredAssetDefinitionCreated(assetDefinitionID, msg.sender, name, isContentPrivate, isContentUnique, contentSchemaHash, now);
-    }
-    
-    function createUnstructuredAssetDefinition(bytes32 assetDefinitionID, string memory name, bool isContentPrivate, bool isContentUnique) public {
-        require(bytes(name).length != 0, "Invalid name");
-        emit UnstructuredAssetDefinitionCreated(assetDefinitionID, msg.sender, name, isContentPrivate, isContentUnique, now);
+    function createAssetDefinition(bytes32 assetDefinitionHash) public {
+        emit AssetDefinitionCreated(assetDefinitionHash);
     }
 
     function createDescribedPaymentDefinition(bytes32 paymentDefinitionID, string memory name, bytes32 descriptionSchemaHash) public {
@@ -167,7 +115,7 @@ contract AssetTrail {
     function createAssetInstance(bytes32 assetInstanceID, bytes32 assetDefinitionID, bytes32 contentHash) public {
         emit AssetInstanceCreated(assetInstanceID, assetDefinitionID, msg.sender, contentHash, now);
     }
-    
+
     function createAssetInstanceBatch(bytes32 batchHash) public {
         emit AssetInstanceBatchCreated(batchHash, msg.sender, now);
     }
@@ -185,10 +133,10 @@ contract AssetTrail {
         require(payment.transferFrom(msg.sender, recipient, amount), "Failed to transfer tokens");
         emit PaymentInstanceCreated(paymentInstanceID, paymentDefinitionID, msg.sender, recipient, amount, now);
     }
-    
+
     function setAssetInstanceProperty(bytes32 assetDefinitionID, bytes32 assetInstanceID, string memory key, string memory value) public {
         require(bytes(key).length > 0, "Invalid key");
         emit AssetInstancePropertySet(assetDefinitionID, assetInstanceID, msg.sender, key, value, now);
     }
-    
+
 }
