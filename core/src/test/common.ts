e9804c012e18b16e4fd6ae563458ea1447027898
Gabriel Indik <gabriel.indik@kaleido.io>
2021-03-01 14:48:36 -0500

    Clean up sandbox at the start of the unit tests
    Signed-off-by: Gabriel Indik <gabriel.indik@kaleido.io>

--- core/src/test/common.ts
+++ core/src/test/common.ts
@@ -2,7 +2,7 @@ import { EventEmitter } from 'events';
 import rimraf from 'rimraf';
 import nock from 'nock';
 import mock from 'mock-require';
-import { promises as fs} from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 import assert from 'assert';
 import request from 'supertest';
@@ -16,48 +16,54 @@ let shutDown: () => void;
 
 class MockWebSocket extends EventEmitter {
 
-    constructor(url: string) {
-      super();
-      assert.strictEqual(url, 'ws://eventstreams.kaleido.io');
-      mockEventStreamWebSocket = this;
-    }
+  constructor(url: string) {
+    super();
+    assert.strictEqual(url, 'ws://eventstreams.kaleido.io');
+    mockEventStreamWebSocket = this;
+  }
 
-    send(message: string) {
-      mockEventStreamWebSocket.emit('send', message);
-    }
+  send(message: string) {
+    mockEventStreamWebSocket.emit('send', message);
+  }
 
-    ping() { }
+  ping() { }
 
-    close() { }
+  close() { }
 
 };
 
 export const setUp = async (protocol: string) => {
-    const sandboxPath = path.join(__dirname, `../../test-resources/sandbox/${protocol}`);
-    try {
-      await fs.mkdir(sandboxPath, {recursive: true});
-    } catch(err) {
-      console.error(err);
-    }
-    await fs.copyFile(path.join(__dirname, '../../test-resources/settings.json'), path.join(sandboxPath, 'settings.json'));
-    await fs.copyFile(path.join(__dirname, `../../test-resources/config-${protocol}.json`), path.join(sandboxPath, 'config.json'));
-    
-    mock('ws', MockWebSocket);
-    mock('socket.io-client', {
-        connect: () => {
-            return mockDocExchangeSocketIO;
-        }
+  await new Promise<void>((resolve, reject) => {
+    rimraf(path.join(__dirname, `../../test-resources/sandbox/${protocol}`), {}, (err) => {
+      if (err) {
+        reject()
+      } else {
+        resolve();
+      }
     });
-    // IPFS
-    nock('https://ipfs.kaleido.io')
+  });
+
+  const sandboxPath = path.join(__dirname, `../../test-resources/sandbox/${protocol}`);
+  await fs.mkdir(sandboxPath, { recursive: true });
+  await fs.copyFile(path.join(__dirname, '../../test-resources/settings.json'), path.join(sandboxPath, 'settings.json'));
+  await fs.copyFile(path.join(__dirname, `../../test-resources/config-${protocol}.json`), path.join(sandboxPath, 'config.json'));
+
+  mock('ws', MockWebSocket);
+  mock('socket.io-client', {
+    connect: () => {
+      return mockDocExchangeSocketIO;
+    }
+  });
+  // IPFS
+  nock('https://ipfs.kaleido.io')
     .post('/api/v0/version')
     .reply(200, { Version: 1 });
 
-    // Doc exchange REST API
-    nock('https://docexchange.kaleido.io')
+  // Doc exchange REST API
+  nock('https://docexchange.kaleido.io')
     .get('/documents')
     .reply(200, { entries: [] });
-    const { promise } = require('../app');
+  const { promise } = require('../app');
   ({ app, shutDown } = await promise);
 
   const eventPromise = new Promise<void>((resolve) => {
@@ -72,7 +78,7 @@ export const setUp = async (protocol: string) => {
 
   await eventPromise;
 
-  if(protocol === 'corda') {
+  if (protocol === 'corda') {
     await setupSampleMembersCorda();
   } else {
     await setupSampleMembersEthereum();
@@ -81,85 +87,80 @@ export const setUp = async (protocol: string) => {
 
 const setupSampleMembersCorda = async () => {
   console.log('Setting up corda members');
-    await request(app)
-      .put('/api/v1/members')
-      .send({
-        address: 'CN=Node of node1 for env1, O=Kaleido, L=Raleigh, C=US',
-        name: 'Test Member 1',
-        assetTrailInstanceID: 'service-id_1',
-        app2appDestination: 'kld://app2app_1',
-        docExchangeDestination: 'kld://docexchange_1'
-      });
-
-    await request(app)
-      .put('/api/v1/members')
-      .send({
-        address: 'CN=Node of node2 for env1, O=Kaleido, L=Raleigh, C=US',
-        name: 'Test Member 2',
-        assetTrailInstanceID: 'service-id_2',
-        app2appDestination: 'kld://app2app_2',
-        docExchangeDestination: 'kld://docexchange_2'
-      });
-  };
-
-  const setupSampleMembersEthereum = async () => {
-    console.log('Setting up ethereum members');
-    nock('https://apigateway.kaleido.io')
-      .post('/registerMember?kld-from=0x0000000000000000000000000000000000000001&kld-sync=true')
-      .reply(200);
-    await request(app)
-      .put('/api/v1/members')
-      .send({
-        address: '0x0000000000000000000000000000000000000001',
-        name: 'Test Member 1',
-        app2appDestination: 'kld://app2app_1',
-        docExchangeDestination: 'kld://docexchange_1'
-      })
-    const eventPromise = new Promise<void>((resolve) => {
-      mockEventStreamWebSocket.once('send', message => {
-        assert.strictEqual(message, '{"type":"ack","topic":"dev"}');
-        resolve();
-      })
-    });
-    const dataMember1: IEventMemberRegistered = {
-      member: '0x0000000000000000000000000000000000000001',
+  await request(app)
+    .put('/api/v1/members')
+    .send({
+      address: 'CN=Node of node1 for env1, O=Kaleido, L=Raleigh, C=US',
       name: 'Test Member 1',
-      assetTrailInstanceID: 'service-instance',
+      assetTrailInstanceID: 'service-id_1',
       app2appDestination: 'kld://app2app_1',
-      docExchangeDestination: 'kld://docexchange_1',
-      timestamp: utils.getTimestamp()
-    }
-    const dataMember2: IEventMemberRegistered =
-    {
-      member: '0x0000000000000000000000000000000000000002',
-      name: 'Test Member 2',
-      assetTrailInstanceID: 'service-instance',
-      app2appDestination: 'kld://app2app_2',
-      docExchangeDestination: 'kld://docexchange_2',
-      timestamp: utils.getTimestamp()
-    };
-    mockEventStreamWebSocket.emit('message', JSON.stringify([{
-      signature: utils.contractEventSignatures.MEMBER_REGISTERED,
-      data: dataMember1,
-      blockNumber: '123',
-      transactionHash: '0x0000000000000000000000000000000000000000000000000000000000000000'
-    }, {
-      signature: utils.contractEventSignatures.MEMBER_REGISTERED,
-      data: dataMember2,
-      blockNumber: '123',
-      transactionHash: '0x0000000000000000000000000000000000000000000000000000000000000000'
-    }]));
-    await eventPromise;
-  };
-
-export const cleanUp = async (protocol: string) => {
-    shutDown(); 
-    await rimraf(path.join(__dirname, `../../test-resources/sandbox/${protocol}`), {}, ( err ) => {
-      if(err) {
-        console.error(err);
-      }
+      docExchangeDestination: 'kld://docexchange_1'
     });
-    mock.stop('ws');
-    mock.stop('socket.io-client');
-    nock.restore();
+
+  await request(app)
+    .put('/api/v1/members')
+    .send({
+      address: 'CN=Node of node2 for env1, O=Kaleido, L=Raleigh, C=US',
+      name: 'Test Member 2',
+      assetTrailInstanceID: 'service-id_2',
+      app2appDestination: 'kld://app2app_2',
+      docExchangeDestination: 'kld://docexchange_2'
+    });
+};
+
+const setupSampleMembersEthereum = async () => {
+  console.log('Setting up ethereum members');
+  nock('https://apigateway.kaleido.io')
+    .post('/registerMember?kld-from=0x0000000000000000000000000000000000000001&kld-sync=true')
+    .reply(200);
+  await request(app)
+    .put('/api/v1/members')
+    .send({
+      address: '0x0000000000000000000000000000000000000001',
+      name: 'Test Member 1',
+      app2appDestination: 'kld://app2app_1',
+      docExchangeDestination: 'kld://docexchange_1'
+    })
+  const eventPromise = new Promise<void>((resolve) => {
+    mockEventStreamWebSocket.once('send', message => {
+      assert.strictEqual(message, '{"type":"ack","topic":"dev"}');
+      resolve();
+    })
+  });
+  const dataMember1: IEventMemberRegistered = {
+    member: '0x0000000000000000000000000000000000000001',
+    name: 'Test Member 1',
+    assetTrailInstanceID: 'service-instance',
+    app2appDestination: 'kld://app2app_1',
+    docExchangeDestination: 'kld://docexchange_1',
+    timestamp: utils.getTimestamp()
+  }
+  const dataMember2: IEventMemberRegistered =
+  {
+    member: '0x0000000000000000000000000000000000000002',
+    name: 'Test Member 2',
+    assetTrailInstanceID: 'service-instance',
+    app2appDestination: 'kld://app2app_2',
+    docExchangeDestination: 'kld://docexchange_2',
+    timestamp: utils.getTimestamp()
+  };
+  mockEventStreamWebSocket.emit('message', JSON.stringify([{
+    signature: utils.contractEventSignatures.MEMBER_REGISTERED,
+    data: dataMember1,
+    blockNumber: '123',
+    transactionHash: '0x0000000000000000000000000000000000000000000000000000000000000000'
+  }, {
+    signature: utils.contractEventSignatures.MEMBER_REGISTERED,
+    data: dataMember2,
+    blockNumber: '123',
+    transactionHash: '0x0000000000000000000000000000000000000000000000000000000000000000'
+  }]));
+  await eventPromise;
+};
+
+export const closeDown = async () => {
+  shutDown();
+  mock.stop('ws');
+  mock.stop('socket.io-client');
+  nock.restore();
 };
