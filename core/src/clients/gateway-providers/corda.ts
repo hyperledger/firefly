33c779d26f09084ee8abe432f0d122095df0d69e
amit panghal <panghalamit1892@gmail.com>
2021-03-01 23:36:34 -0500

    address self review comments
    Signed-off-by: Amit Panghal <amit.panghal@kaleido.io>

--- core/src/clients/gateway-providers/corda.ts
+++ core/src/clients/gateway-providers/corda.ts
@@ -83,8 +83,8 @@ export const setAssetInstanceProperty = async (assetDefinitionID: string, assetI
         password: config.apiGateway.auth?.password ?? config.appCredentials.password
       },
       data: {
-        assetDefinitionID,
-        assetInstanceID,
+        assetDefinitionID: utils.uuidToHex(assetDefinitionID),
+        assetInstanceID: utils.uuidToHex(assetInstanceID),
         key,
         value,
         participants
