00987007080775c8d13780f1ff823909402aa425
jebonfig <joe.bonfiglio@kaleido.io>
2021-05-28 10:50:49 -0400

    remove ERC20 requirement from the Firefly contract

--- solidity_firefly/contracts/Firefly.sol
+++ solidity_firefly/contracts/Firefly.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
-pragma solidity >=0.6.0 <0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity >=0.6.0 <0.9.0;
 
 contract Firefly {
 
@@ -13,12 +11,6 @@ contract Firefly {
         bytes32 batchId,
         bytes32 payloadRef
     );
-
-    ERC20 payment;
-
-    constructor(address paymentContract) public {
-        payment = ERC20(paymentContract);
-    }
 
     function broadcastBatch(bytes32 txnId, bytes32 batchId, bytes32 payloadRef) public {
         emit BroadcastBatch(msg.sender, block.timestamp, txnId, batchId, payloadRef);
