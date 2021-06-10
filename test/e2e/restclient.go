33a78a33e605f1bdc50c7b9d6a23e9ab40468712
Andrew Richardson <andrew.richardson@kaleido.io>
2021-06-10 16:57:57 -0400

    Fix E2E test
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- test/e2e/restclient.go
+++ test/e2e/restclient.go
@@ -22,9 +22,10 @@ import (
 )
 
 var (
-	urlGetNamespaces     = "/namespaces"
-	urlBroadcastDatatype = "/namespaces/default/broadcast/datatype"
-	urlGetData           = "/namespaces/default/data"
+	urlGetNamespaces    = "/namespaces"
+	urlGetMessages      = "/namespaces/default/messages"
+	urlBroadcastMessage = "/namespaces/default/broadcast/message"
+	urlGetData          = "/namespaces/default/data"
 )
 
 func GetNamespaces(client *resty.Client) (*resty.Response, error) {
@@ -33,37 +34,18 @@ func GetNamespaces(client *resty.Client) (*resty.Response, error) {
 		Get(urlGetNamespaces)
 }
 
-func BroadcastDatatype(client *resty.Client, name string) (*resty.Response, error) {
+func GetMessages(client *resty.Client) (*resty.Response, error) {
 	return client.R().
-		SetBody(fftypes.Datatype{
-			Name:    name,
-			Version: "1",
-			Value: fftypes.Byteable(`
-			{
-				"type": "object",
-				"properties": {
-					"property1": {
-						"type": "string"
-					}
-				}// Copyright © 2021 Kaleido, Inc.
-				//
-				// SPDX-License-Identifier: Apache-2.0
-				//
-				// Licensed under the Apache License, Version 2.0 (the "License");
-				// you may not use this file except in compliance with the License.
-				// You may obtain a copy of the License at
-				//
-				//     http://www.apache.org/licenses/LICENSE-2.0
-				//
-				// Unless required by applicable law or agreed to in writing, software
-				// distributed under the License is distributed on an "AS IS" BASIS,
-				// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-				// See the License for the specific language governing permissions and
-				// limitations under the License.
-				
-				
-			}`),
-		}).Post(urlBroadcastDatatype)
+		SetResult(&[]fftypes.Message{}).
+		Get(urlGetMessages)
+}
+
+func BroadcastMessage(client *resty.Client, data *fftypes.DataRefOrValue) (*resty.Response, error) {
+	return client.R().
+		SetBody(fftypes.MessageInput{
+			InputData: fftypes.InputData{data},
+		}).
+		Post(urlBroadcastMessage)
 }
 
 func GetData(client *resty.Client) (*resty.Response, error) {
