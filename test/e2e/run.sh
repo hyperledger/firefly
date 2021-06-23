ea40b627d2cc003b0dd9ed234ad13038645ba22e
Andrew Richardson <andrew.richardson@kaleido.io>
2021-06-23 14:29:21 -0400

    E2E script cleanup
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash -x
-set -eo pipefail
-
-CWD=$(dirname "$0")
-CLI="ff -v --ansi never"
-STACK_DIR=~/.firefly/stacks
-STACK_NAME=firefly-e2e
-STACK_FILE=$STACK_DIR/$STACK_NAME/stack.json
 
 if [ -z "${DOWNLOAD_CLI}" ]; then
   DOWNLOAD_CLI=true
@@ -19,20 +12,25 @@ if [ -z "${BUILD_FIREFLY}" ]; then
   BUILD_FIREFLY=true
 fi
 
+set -euo pipefail
+
+CWD=$(dirname "$0")
+CLI="ff -v --ansi never"
+STACK_DIR=~/.firefly/stacks
+STACK_NAME=firefly-e2e
+STACK_FILE=$STACK_DIR/$STACK_NAME/stack.json
+
 cd $CWD
 
-if [ "$BUILD_FIREFLY" == "true" ]
-then
+if [ "$BUILD_FIREFLY" == "true" ]; then
 	docker build -t ghcr.io/hyperledger-labs/firefly:latest ../..
 fi
 
-if [ "$DOWNLOAD_CLI" == "true" ]
-then
+if [ "$DOWNLOAD_CLI" == "true" ]; then
 	go install github.com/hyperledger-labs/firefly-cli/ff@latest
 fi
 
-if [ "$CREATE_STACK" == "true" ]
-then
+if [ "$CREATE_STACK" == "true" ]; then
 	$CLI remove -f $STACK_NAME || true
 	$CLI init $STACK_NAME 2
 	$CLI start -n $STACK_NAME
