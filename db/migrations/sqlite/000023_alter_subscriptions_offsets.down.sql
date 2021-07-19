73f26e76cf133a45987f7ba7307f7520d9a59f77
Peter Broadhurst <peter.broadhurst@kaleido.io>
2021-07-19 09:16:50 -0400

    Truncate offsets on migrations, and re-instate columns/indexes on down
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- db/migrations/sqlite/000023_alter_subscriptions_offsets.down.sql
+++ db/migrations/sqlite/000023_alter_subscriptions_offsets.down.sql
@@ -1,1 +1,14 @@
 ALTER TABLE subscriptions DROP COLUMN updated;
+
+-- We change the primary key by which we access the data, so truncate the table
+-- Meaning offsets for subscriptions will be reset going down
+DELETE FROM offsets;
+
+DROP INDEX offsets_id;
+DROP INDEX offsets_unique;
+
+ALTER TABLE offsets ADD id UUID NOT NULL;
+ALTER TABLE offsets ADD namespace VARCHAR(64) NOT NULL;
+
+CREATE UNIQUE INDEX offsets_id ON offsets(id);
+CREATE UNIQUE INDEX offsets_unique ON offsets(otype,namespace,name);
