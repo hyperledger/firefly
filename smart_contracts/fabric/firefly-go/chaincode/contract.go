fdeda3abfec74fc08f2c457b55138761246c28fe
Jim Zhang <jim.zhang@kaleido.io>
2021-08-23 14:32:48 -0400

    Added initial impl for the Firefly base chaincode supporting batch pinning
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

---
+++ smart_contracts/fabric/firefly-go/chaincode/contract.go
@@ -0,0 +1,55 @@
+package chaincode
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+// SmartContract provides functions for managing an Asset
+type SmartContract struct {
+	contractapi.Contract
+}
+
+type BatchPinEvent struct {
+	Signer     string               `json:"signer"`
+	Timestamp  *timestamp.Timestamp `json:"timestamp"`
+	Namespace  string               `json:"namespace"`
+	Uuids      string               `json:"uuids"`
+	BatchHash  string               `json:"batchhash"`
+	PayloadRef string               `json:"payloadRef"`
+	Contexts   []string             `json:"contexts"`
+}
+
+func (s *SmartContract) PinBatch(ctx contractapi.TransactionContextInterface, namespace, uuids, batchHash, payloadRef string, contexts []string) error {
+	cid := ctx.GetClientIdentity()
+	id, err := cid.GetID()
+	if err != nil {
+		return fmt.Errorf("Failed to obtain client identity's ID: %s", err)
+	}
+	mspId, err := cid.GetMSPID()
+	if err != nil {
+		return fmt.Errorf("Failed to obtain client identity's MSP ID: %s", err)
+	}
+	timestamp, err := ctx.GetStub().GetTxTimestamp()
+	if err != nil {
+		return fmt.Errorf("Failed to get transaction timestamp: %s", err)
+	}
+	event := BatchPinEvent{
+		Signer:     fmt.Sprintf("%s-%s", id, mspId),
+		Timestamp:  timestamp,
+		Namespace:  namespace,
+		Uuids:      uuids,
+		BatchHash:  batchHash,
+		PayloadRef: payloadRef,
+		Contexts:   contexts,
+	}
+	bytes, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("Failed to marshal event: %s", err)
+	}
+	ctx.GetStub().SetEvent("BatchPin", bytes)
+	return nil
+}
