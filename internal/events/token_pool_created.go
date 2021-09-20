f616efef58dca87e860708716802adcb5e6d20f8
Andrew Richardson <andrew.richardson@kaleido.io>
2021-09-20 11:39:19 -0400

    Factor out "PersistTransaction" as a common database utility
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- internal/events/token_pool_created.go
+++ internal/events/token_pool_created.go
@@ -30,7 +30,7 @@ func (em *eventManager) persistTokenPoolTransaction(ctx context.Context, pool *f
 		log.L(ctx).Errorf("Invalid token pool '%s'. Missing ID (%v) or transaction ID (%v)", pool.ID, pool.ID, pool.TX.ID)
 		return false, nil // this is not retryable
 	}
-	return em.persistTransaction(ctx, &fftypes.Transaction{
+	return database.PersistTransaction(ctx, em.database, &fftypes.Transaction{
 		ID: pool.TX.ID,
 		Subject: fftypes.TransactionSubject{
 			Namespace: pool.Namespace,
