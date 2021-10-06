6f62a4aba3d2cdafef5e1cec4932875fc6dd1ce0
Jim Zhang <jim.zhang@kaleido.io>
2021-10-06 15:56:30 -0400

    Fixed empty string delimiter in SQL migration scripts
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

--- db/migrations/sqlite/000030_create_key_fields.up.sql
+++ db/migrations/sqlite/000030_create_key_fields.up.sql
@@ -1,8 +1,8 @@
 ALTER TABLE batches ADD COLUMN "key" VARCHAR(1024);
-UPDATE batches SET "key" = "";
+UPDATE batches SET "key" = '';
 
 ALTER TABLE messages ADD COLUMN "key" VARCHAR(1024);
-UPDATE messages SET "key" = "";
+UPDATE messages SET "key" = '';
 
 ALTER TABLE tokenpool ADD COLUMN "key" VARCHAR(1024);
-UPDATE tokenpool SET "key" = "";
+UPDATE tokenpool SET "key" = '';
