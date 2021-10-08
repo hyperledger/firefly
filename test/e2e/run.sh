79966f780f1d7c0ed18636f994069e96aa5c5161
Peter Broadhurst <peter.broadhurst@kaleido.io>
2021-10-08 07:52:31 -0400

    Capture logs if startup or earlier steps fail
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
 
-set -eo pipefail
+set -o pipefail
 
 CWD=$(dirname "$0")
 CLI="ff -v --ansi never"
 STACK_DIR=~/.firefly/stacks
 STACK_NAME=firefly-e2e
+RC=0
 
 if [ -z "${DOWNLOAD_CLI}" ]; then
   DOWNLOAD_CLI=true
@@ -30,33 +31,47 @@ fi
 
 cd $CWD
 
-if [ "$CREATE_STACK" == "true" ]; then
+if [ $RC -eq 0 ] && [ "$CREATE_STACK" == "true" ]; then
 	$CLI remove -f $STACK_NAME || true
+  RC=$?
 fi
 
-if [ "$BUILD_FIREFLY" == "true" ]; then
+if [ $RC -eq 0 ] && [ "$BUILD_FIREFLY" == "true" ]; then
 	docker build -t ghcr.io/hyperledger/firefly:latest ../..
+  RC=$?
 fi
 
-if [ "$DOWNLOAD_CLI" == "true" ]; then
+if [ $RC -eq 0 ] && [ "$DOWNLOAD_CLI" == "true" ]; then
 	go install github.com/hyperledger/firefly-cli/ff@latest
+  RC=$?
 fi
 
 if [ "$CREATE_STACK" == "true" ]; then
-	$CLI init --database $DATABASE_TYPE $STACK_NAME 2
-	$CLI start -nb $STACK_NAME
+  if [ $RC -eq 0 ]; then
+	  $CLI init --database $DATABASE_TYPE $STACK_NAME 2
+    RC=$?
+  fi
+
+  if [ $RC -eq 0 ]; then
+  	$CLI start -nb $STACK_NAME
+    RC=$?
+  fi
 fi
 
-$CLI info $STACK_NAME
+if [ $RC -eq 0 ]; then
+  $CLI info $STACK_NAME
+  RC=$?
+fi
 
 export STACK_FILE
 
-set +e
-go clean -testcache && go test -v .
-RC=$?
+if [ $RC -eq 0 ]; then
+  go clean -testcache && go test -v .
+  RC=$?
+fi
 
 WORKDIR=${GITHUB_WORKSPACE}
-if [ -z "$WORKDIR" ]; then WORKDIR=.; fi
+if [ -z "$WORKDIR" ]; then WORKDIR=$CWD; fi
 
 mkdir -p "${WORKDIR}/containerlogs"
 $CLI logs $STACK_NAME > "${WORKDIR}/containerlogs/logs.txt"
