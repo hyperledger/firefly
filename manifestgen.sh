908331bb584a2fb7cc2833e44470183d6d0e8b22
Nicko Guyer <nicko.guyer@kaleido.io>
2021-10-11 16:55:28 -0400

    Add manifest for CLI to pull specific version sets
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

---
+++ manifestgen.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright © 2021 Kaleido, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script will automatically update the manifest.yaml file with the
+# latest releases of all FireFly microservice dependencies
+
+rm -f manifest.yaml
+
+echo $'# SPDX-License-Identifier: Apache-2.0\n' >> manifest.yaml
+echo $'# FireflyVersion Manifest' >> manifest.yaml
+echo $'# This file describes the version of each microservice that should be used with this version of FireFly\n' >> manifest.yaml
+
+declare -a arr=(
+    "ethconnect"
+    "fabconnect"
+    "data-exchange-https"
+    "tokens-erc-1155"
+)
+
+for i in "${arr[@]}"
+do
+   echo "$i: ghcr.io/hyperledger/firefly-$i:$(curl https://api.github.com/repos/hyperledger/firefly-$i/releases/latest -s | jq .tag_name -r)" >> manifest.yaml
+done
