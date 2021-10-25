d16d316d70a446366df0f2a44354a2b914a971e4
Nicko Guyer <nicko.guyer@kaleido.io>
2021-10-25 15:03:39 -0400

    Update manifestgen.sh to pull latest commit to main
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- manifestgen.sh
+++ manifestgen.sh
@@ -19,6 +19,18 @@
 # This script will automatically update the manifest.json file with the
 # latest releases of all FireFly microservice dependencies
 
+USE_HEAD=false
+
+# If you pass the string "head" as an argument to this script, it will 
+# get the latest build, straight off the main branch. Otherwise it will
+# get the most recent release that is not a pre-release.
+if [[ $# -eq 1 ]] ; then
+    if [[ $1 -eq "head" ]] ; then
+        echo 'using head'
+        USE_HEAD=true
+    fi
+fi
+
 rm -f manifest.json
 
 SERVICES=(
@@ -36,15 +48,28 @@ do
     echo "  \"${SERVICES[$i]}\": {" >> manifest.json
     echo "    \"image\": \"ghcr.io/hyperledger/firefly-${SERVICES[$i]}\"," >> manifest.json
 
-    # Query GitHub API the latest release version
-    TAG=$(curl https://api.github.com/repos/hyperledger/firefly-${SERVICES[$i]}/releases/latest -s | jq .tag_name -r)
+    if [ $USE_HEAD = false ] ; then
+        # Query GitHub API the latest release version
+        TAG=$(curl https://api.github.com/repos/hyperledger/firefly-${SERVICES[$i]}/releases/latest -s | jq .tag_name -r)
+    else
+        # Otherwise, pull the newest built image straight off the main branch
+        TAG="head"
+    fi
     
     # Attempt to pull the image from GitHub Container Repository
     docker pull ghcr.io/hyperledger/firefly-${SERVICES[$i]}:$TAG
     # Get the SHA of the downloaded image
     SHA=$(docker inspect --format='{{index .RepoDigests 0}}' ghcr.io/hyperledger/firefly-${SERVICES[$i]}:$TAG | cut -d ':' -f 2)
+    # Get the tag / build number name of this image from its label
+    TAG_LABEL=$(docker inspect --format='{{index .Config.Labels "tag"}}' ghcr.io/hyperledger/firefly-${SERVICES[$i]}:$TAG)
 
-    echo "    \"tag\": \"$TAG\"," >> manifest.json
+    # If the tag / build number wasn't set in the label, use whatever docker tag we fetched
+    # This is done for backwards compatability, because not all images have labels yet
+    if [ -z "$TAG_LABEL" ]; then
+        TAG_LABEL=$TAG
+    fi
+
+    echo "    \"tag\": \"$TAG_LABEL\"," >> manifest.json
     echo "    \"sha\": \"$SHA\"" >> manifest.json
     if [[ $(($i + 1)) -eq ${SERVICE_COUNT} ]]
     then
