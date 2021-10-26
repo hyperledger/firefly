84922c9ab0b5b481c12764f48c531282028a79e2
Peter Broadhurst <peter.broadhurst@kaleido.io>
2021-10-26 17:43:11 -0400

    Move to env vars into scripts not templating
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- deploy/charts/firefly/scripts/ff-registration.sh
+++ deploy/charts/firefly/scripts/ff-registration.sh
@@ -2,7 +2,7 @@
 
 apk add curl jq
 
-while ! STATUS=$(curl http://{{ include "firefly.fullname" . }}:{{ .Values.core.service.httpPort }}/api/v1/status); do
+while ! STATUS=$(curl ${FF_URL}/api/v1/status); do
   echo "Waiting for FireFly..."
   sleep 5
 done
@@ -12,7 +12,7 @@ if [ `echo $STATUS | jq -r .registered` != "true" ]; then
   echo "Registering organization"
   HTTP_CODE=`curl --silent --output /dev/stderr --write-out "%{http_code}" \
     -X POST -d '{}' -H 'Content-Type: application/json' \
-    "http://{{ include "firefly.fullname" . }}:{{ .Values.core.service.httpPort }}/api/v1/network/organizations/self?confirm"`
+    "${FF_URL}/api/v1/network/organizations/self?confirm"`
   if [ "$HTTP_CODE" -ne 200 ]; then
     echo "Failed to register with code ${HTTP_CODE}"
     exit 1
@@ -21,7 +21,7 @@ if [ `echo $STATUS | jq -r .registered` != "true" ]; then
   echo "Registering node"
   HTTP_CODE=`curl --silent --output /dev/stderr --write-out "%{http_code}" \
     -X POST -d '{}' -H 'Content-Type: application/json' \
-    "http://{{ include "firefly.fullname" . }}:{{ .Values.core.service.httpPort }}/api/v1/network/nodes/self?confirm"`
+    "${FF_URL}/api/v1/network/nodes/self?confirm"`
   if [ "$HTTP_CODE" -ne 200 ]; then
     echo "Failed to register with code ${HTTP_CODE}"
     exit 1
