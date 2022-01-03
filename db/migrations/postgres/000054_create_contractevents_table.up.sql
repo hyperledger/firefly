1124b930f9af19095b246bb2de9ae6336f7d97ca
Nicko Guyer <nicko.guyer@kaleido.io>
2022-01-03 12:45:54 -0500

    Add postgres migrations for onchain-logic
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- db/migrations/postgres/000054_create_contractevents_table.up.sql
+++ db/migrations/postgres/000054_create_contractevents_table.up.sql
@@ -1,12 +1,12 @@
 BEGIN;
 CREATE TABLE contractevents (
-  seq              INTEGER         PRIMARY KEY AUTOINCREMENT,
+  seq              SERIAL          PRIMARY KEY,
   id               UUID            NOT NULL,
   namespace        VARCHAR(64)     NOT NULL,
   name             VARCHAR(1024)   NOT NULL,
   subscription_id  UUID            NOT NULL,
   outputs          BYTEA,
   info             BYTEA,
-  created          BIGINT          NOT NULL
+  timestamp        BIGINT          NOT NULL
 );
 COMMIT;
