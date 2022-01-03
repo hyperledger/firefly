54297f1cd2d97e967c88c2f62112f2b8d4f2e853
Drew Marshburn <drewmarshburn@gmail.com>
2022-01-03 14:17:25 -0500

    Have DB creation script account for username
    Signed-off-by: Drew Marshburn <drewmarshburn@gmail.com>

--- deploy/charts/firefly/scripts/ff-db-migrations.sh
+++ deploy/charts/firefly/scripts/ff-db-migrations.sh
@@ -3,29 +3,34 @@
 # Install deps
 apk add postgresql-client curl jq
 
+echo "Provided connection string: '${PSQL_URL}'"
+
 # Extract the database name from the end of the PSQL URL, and check it's there
-DB_NAME=`echo ${PSQL_URL} | sed 's/^.*\///'`
-COLONS=`echo -n $DB_NAME | sed 's/[^:]//g'`
+DB_PARAMS=`echo ${PSQL_URL} | sed 's!^.*/!!'`
+DB_NAME=`echo ${DB_PARAMS} | sed 's!?.*!!'`
 echo "Database name: '${DB_NAME}'"
+USER_NAME=`echo ${PSQL_URL} | sed 's!^.*//!!' | sed 's!:.*$!!'`
+echo "Username: '${USER_NAME}'"
+COLONS=`echo -n $DB_NAME | sed 's/[^:]//g'`
 if [ -z "${DB_NAME}" ] || [ -n "${COLONS}" ]
 then
-  echo "Postgres URL does not appear to contain a database name"
+  echo "Error: Postgres URL does not appear to contain a database name (required)."
   exit 1
 fi
 
-# Build a URL that doesn't have the database name
-PSQL_URL_NO_DB=`echo ${PSQL_URL} | sed "s/\/${DB_NAME}//"`
-
-# Check we can connect to the PSQL Server
-until psql -c "SELECT 1;" ${PSQL_URL_NO_DB}; do
-  echo "Waiting for database..."
+# Check we can connect to the PSQL server using the default "postgres" database
+PSQL_SERVER=`echo ${PSQL_URL} | sed "s!${DB_PARAMS}!!"`postgres
+echo "PSQL server URL: '${PSQL_SERVER}'"
+until psql -c "SELECT 1;" ${PSQL_SERVER}; do
+  echo "Waiting for PSQL server connection..."
   sleep 1
 done
 
 # Create the database if it doesn't exist
-if ! psql -c "SELECT datname FROM pg_database WHERE datname = '${DB_NAME}';" ${PSQL_URL_NO_DB} | grep ${DB_NAME}
+if ! psql -c "SELECT datname FROM pg_database WHERE datname = '${DB_NAME}';" ${PSQL_SERVER} | grep ${DB_NAME}
 then
-  psql -c "CREATE DATABASE ${DB_NAME};" ${PSQL_URL_NO_DB}
+  echo "Database '${DB_NAME}' does not exist; creating."
+  psql -c "CREATE DATABASE \"${DB_NAME}\" WITH OWNER \"${USER_NAME}\";" ${PSQL_SERVER}
 fi
 
 # Wait for the database itself to be available
