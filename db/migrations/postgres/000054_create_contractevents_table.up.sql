f53d90442dce3e157110d15c588e80a997f14f0c
Peter Broadhurst <peter.broadhurst@kaleido.io>
2022-01-11 23:31:01 -0500

    Move new JSONAny columns to TEXT too
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- db/migrations/postgres/000054_create_contractevents_table.up.sql
+++ db/migrations/postgres/000054_create_contractevents_table.up.sql
@@ -5,8 +5,8 @@ CREATE TABLE contractevents (
   namespace        VARCHAR(64)     NOT NULL,
   name             VARCHAR(1024)   NOT NULL,
   subscription_id  UUID            NOT NULL,
-  outputs          BYTEA,
-  info             BYTEA,
+  outputs          TEXT,
+  info             TEXT,
   timestamp        BIGINT          NOT NULL
 );
 CREATE UNIQUE INDEX contractevents_name ON contractevents(namespace,name);
--- db/migrations/sqlite/000054_create_contractevents_table.up.sql
+++ db/migrations/sqlite/000054_create_contractevents_table.up.sql
@@ -4,8 +4,8 @@ CREATE TABLE contractevents (
   namespace        VARCHAR(64)     NOT NULL,
   name             VARCHAR(1024)   NOT NULL,
   subscription_id  UUID            NOT NULL,
-  outputs          BYTEA,
-  info             BYTEA,
+  outputs          TEXT,
+  info             TEXT,
   timestamp        BIGINT          NOT NULL
 );
 
