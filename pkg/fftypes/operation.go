6eb5027bbc6a656c8f9dff77399614b7dc7bf1e2
Andrew Richardson <andrew.richardson@kaleido.io>
2022-02-04 13:37:24 -0500

    Rename operation for blockchain invoke
    The convention for all other operations is to prefix them with the name of
    the target plugin ("blockchain" in this case).
    
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- pkg/fftypes/operation.go
+++ pkg/fftypes/operation.go
@@ -24,6 +24,8 @@ type OpType = FFEnum
 var (
 	// OpTypeBlockchainBatchPin is a blockchain transaction to pin a batch
 	OpTypeBlockchainBatchPin OpType = ffEnum("optype", "blockchain_batch_pin")
+	// OpTypeBlockchainInvoke is a smart contract invoke
+	OpTypeBlockchainInvoke OpType = ffEnum("optype", "blockchain_invoke")
 	// OpTypePublicStorageBatchBroadcast is a public storage operation to store broadcast data
 	OpTypePublicStorageBatchBroadcast OpType = ffEnum("optype", "publicstorage_batch_broadcast")
 	// OpTypeDataExchangeBatchSend is a private send
@@ -36,8 +38,6 @@ var (
 	OpTypeTokenAnnouncePool OpType = ffEnum("optype", "token_announce_pool")
 	// OpTypeTokenTransfer is a token transfer
 	OpTypeTokenTransfer OpType = ffEnum("optype", "token_transfer")
-	// OpTypeContractInvoke is a smart contract invoke
-	OpTypeContractInvoke OpType = ffEnum("optype", "contract_invoke")
 )
 
 // OpStatus is the current status of an operation
