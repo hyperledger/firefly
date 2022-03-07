f8a6d0f48f6d7fcc16bbed90bcd0d4dbd2fa218f
Alex Shorsher <alex.shorsher@kaleido.io>
2022-03-07 09:16:18 -0500

    subscription upgrade migrations
     - up migrations for subscription filter changes
    
    Signed-off-by: Alex Shorsher <alex.shorsher@kaleido.io>

--- db/migrations/postgres/000070_add_subscriptions_filters.up.sql
+++ db/migrations/postgres/000070_add_subscriptions_filters.up.sql
@@ -1,5 +1,6 @@
 BEGIN;
 ALTER TABLE subscriptions ADD COLUMN filters TEXT;
+UPDATE subscriptions SET filters='{"events":"' || filter_events || '","message":{"topics":"' || filter_topics || '","tag":"' || filter_tag || '","group":"' || filter_group || '"}}';
 ALTER TABLE subscriptions DROP COLUMN filter_events;
 ALTER TABLE subscriptions DROP COLUMN filter_topics;
 ALTER TABLE subscriptions DROP COLUMN filter_tag;
--- db/migrations/sqlite/000070_add_subscriptions_filters.up.sql
+++ db/migrations/sqlite/000070_add_subscriptions_filters.up.sql
@@ -1,4 +1,5 @@
 ALTER TABLE subscriptions ADD COLUMN filters TEXT;
+UPDATE subscriptions SET filters='{"events":"' || filter_events || '","message":{"topics":"' || filter_topics || '","tag":"' || filter_tag || '","group":"' || filter_group || '"}}';
 ALTER TABLE subscriptions DROP COLUMN filter_events;
 ALTER TABLE subscriptions DROP COLUMN filter_topics;
 ALTER TABLE subscriptions DROP COLUMN filter_tag;
