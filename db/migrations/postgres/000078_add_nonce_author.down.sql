25a2b21893c5d32c117977db4ea1bb52aa3294ad
Peter Broadhurst <peter.broadhurst@kaleido.io>
2022-03-30 15:04:38 -0400

    Remove cruft from migration
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- db/migrations/postgres/000078_add_nonce_author.down.sql
+++ db/migrations/postgres/000078_add_nonce_author.down.sql
@@ -5,7 +5,6 @@ DROP INDEX nonces_hash;
 ALTER TABLE nonces RENAME COLUMN "hash" TO "context";
 ALTER TABLE nonces ADD COLUMN group_hash CHAR(64);
 ALTER TABLE nonces ADD COLUMN topic VARCHAR(64);
-ALTER TABLE nonces ADD COLUMN
 
 CREATE INDEX nonces_context ON nonces(context);
 CREATE INDEX nonces_group ON nonces(group_hash);
