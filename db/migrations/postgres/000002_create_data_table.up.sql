83c28ea8369624b926e387998b6b6de21a9b153a
Nicko Guyer <nicko.guyer@kaleido.io>
2022-04-09 22:44:36 -0400

    Fix postgres migrations and add to PR checks
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- db/migrations/postgres/000002_create_data_table.up.sql
+++ db/migrations/postgres/000002_create_data_table.up.sql
@@ -9,9 +9,11 @@ CREATE TABLE data (
   hash             CHAR(64)        NOT NULL,
   created          BIGINT          NOT NULL,
   value            TEXT            NOT NULL,
-  blobstore        BOOLEAN         NOT NULL
+  blob_hash        CHAR(64),
+  blob_public      VARCHAR(1024)
 );
 CREATE UNIQUE INDEX data_id ON data(id);
 CREATE INDEX data_hash ON data(namespace,hash);
 CREATE INDEX data_created ON data(namespace,created);
+CREATE INDEX data_blobs ON data(blob_hash);
 COMMIT;
