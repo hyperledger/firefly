e19f2a470ccd951a8242ff556ba4eeb6d1cefaa0
Andrew Richardson <andrew.richardson@kaleido.io>
2022-06-09 14:40:29 -0400

    Tweak namespace parsing from dx
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- internal/dataexchange/ffdx/dxevent.go
+++ internal/dataexchange/ffdx/dxevent.go
@@ -156,10 +156,14 @@ func (h *FFDX) dispatchEvent(msg *wsEvent) {
 		var hash *fftypes.Bytes32
 		hash, err = fftypes.ParseBytes32(h.ctx, msg.Hash)
 		if err == nil {
+			var ns string
 			pathParts := strings.Split(msg.Path, "/")
+			if len(pathParts) >= 2 {
+				ns = pathParts[len(pathParts)-2]
+			}
 			e.dxType = dataexchange.DXEventTypePrivateBlobReceived
 			e.privateBlobReceived = &dataexchange.PrivateBlobReceived{
-				Namespace:  pathParts[0],
+				Namespace:  ns,
 				PeerID:     msg.Sender,
 				Hash:       *hash,
 				Size:       msg.Size,
