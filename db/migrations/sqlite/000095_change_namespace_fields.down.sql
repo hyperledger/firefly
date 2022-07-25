54c240983152b6e8775a33172492232949c4bd0f
Andrew Richardson <andrew.richardson@kaleido.io>
2022-07-25 11:40:51 -0400

    Allow tx IDs to be reused on different namespaces
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- db/migrations/sqlite/000095_change_namespace_fields.down.sql
+++ db/migrations/sqlite/000095_change_namespace_fields.down.sql
@@ -5,3 +5,8 @@ ALTER TABLE namespaces ADD COLUMN id UUID;
 ALTER TABLE namespaces ADD COLUMN message_id UUID;
 ALTER TABLE namespaces ADD COLUMN ntype VARCHAR(64);
 ALTER TABLE namespaces DROP COLUMN remote_name;
+
+DROP INDEX transactions_id;
+CREATE UNIQUE INDEX transactions_id ON transactions(id);
+DROP INDEX operations_id;
+CREATE UNIQUE INDEX operations_id ON operations(id);
