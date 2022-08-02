ba790af44d986409aa358d02227cb7b8f888fad9
Alex Shorsher <alex.shorsher@kaleido.io>
2022-08-02 13:54:59 -0400

    add e2e tests for tokens-only namespaces
    Signed-off-by: Alex Shorsher <alex.shorsher@kaleido.io>

---
+++ test/e2e/gateway/tokens_only.go
@@ -0,0 +1,144 @@
+// Copyright © 2022 Kaleido, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gateway
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/hyperledger/firefly-common/pkg/fftypes"
+	"github.com/hyperledger/firefly/pkg/core"
+	"github.com/hyperledger/firefly/test/e2e"
+	"github.com/hyperledger/firefly/test/e2e/client"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type TokensOnlyTestSuite struct {
+	suite.Suite
+	testState   *testState
+	connector   string
+	db          string
+	key         string
+	adminHost1  string
+	configFile1 string
+}
+
+func (suite *TokensOnlyTestSuite) SetupSuite() {
+	suite.testState = beforeE2ETest(suite.T())
+	stack := e2e.ReadStack(suite.T())
+	stackState := e2e.ReadStackState(suite.T())
+
+	adminProtocol1 := schemeHTTP
+	if stack.Members[0].UseHTTPS {
+		adminProtocol1 = schemeHTTPS
+	}
+	suite.adminHost1 = fmt.Sprintf("%s://%s:%d", adminProtocol1, stack.Members[0].FireflyHostname, stack.Members[0].ExposedAdminPort)
+
+	stackDir := os.Getenv("STACK_DIR")
+	if stackDir == "" {
+		suite.T().Fatal("STACK_DIR must be set")
+	}
+	suite.configFile1 = filepath.Join(stackDir, "runtime", "config", "firefly_core_0.yml")
+
+	suite.connector = stack.TokenProviders[0]
+	suite.db = stack.Database
+	account := stackState.Accounts[0].(map[string]interface{})
+	suite.key = account["address"].(string)
+}
+
+func (suite *TokensOnlyTestSuite) BeforeTest(suiteName, testName string) {
+	suite.testState = beforeE2ETest(suite.T())
+}
+
+func (suite *TokensOnlyTestSuite) AfterTest(suiteName, testName string) {
+	e2e.VerifyAllOperationsSucceeded(suite.T(), []*client.FireFlyClient{suite.testState.client1}, suite.testState.startTime)
+}
+
+func (suite *TokensOnlyTestSuite) TestTokensOnlyNamespaces() {
+	defer suite.testState.Done()
+	testNamespace := randomName(suite.T())
+	suite.T().Logf("Test namespace: %s", testNamespace)
+
+	namespaceInfo := map[string]interface{}{
+		"name": testNamespace,
+		"assets": map[string]interface{}{
+			"manager": map[string]interface{}{
+				"keynormalization": "none",
+			},
+		},
+		"plugins": []string{"database0", suite.connector},
+	}
+
+	// Add the new namespace
+	data1 := readConfig(suite.T(), suite.configFile1)
+	addNamespace(data1, namespaceInfo)
+	writeConfig(suite.T(), suite.configFile1, data1)
+
+	admin1 := client.NewResty(suite.T())
+	admin1.SetBaseURL(suite.adminHost1 + "/spi/v1")
+
+	resetFireFly(suite.T(), admin1)
+	e2e.PollForUp(suite.T(), suite.testState.client1)
+
+	client1 := client.NewFireFly(suite.T(), suite.testState.client1.Hostname, testNamespace)
+	eventNames := "token_pool_confirmed|token_transfer_confirmed"
+	queryString := fmt.Sprintf("namespace=%s&ephemeral&autoack&filter.events=%s", testNamespace, eventNames)
+	received1 := e2e.WsReader(client1.WebSocket(suite.T(), queryString, nil))
+
+	// Attempt async token operations on new namespace
+	poolName := fmt.Sprintf("pool_%s", randomName(suite.T()))
+	suite.T().Logf("Pool name: %s", poolName)
+
+	pool := &core.TokenPool{
+		Name: poolName,
+		Key:  suite.key,
+		Type: core.TokenTypeFungible,
+	}
+	poolResp := client1.CreateTokenPool(suite.T(), pool, false)
+	poolID := poolResp.ID
+
+	e2e.WaitForEvent(suite.T(), received1, core.EventTypePoolConfirmed, poolID)
+	pools := client1.GetTokenPools(suite.T(), suite.testState.startTime)
+	assert.Equal(suite.T(), 1, len(pools))
+	assert.Equal(suite.T(), testNamespace, pools[0].Namespace)
+	assert.Equal(suite.T(), suite.connector, pools[0].Connector)
+	assert.Equal(suite.T(), poolName, pools[0].Name)
+	assert.Equal(suite.T(), core.TokenTypeFungible, pools[0].Type)
+	assert.NotEmpty(suite.T(), pools[0].Locator)
+
+	transfer := &core.TokenTransferInput{
+		TokenTransfer: core.TokenTransfer{Amount: *fftypes.NewFFBigInt(1), Key: suite.key},
+		Pool:          poolName,
+	}
+	transferOut := client1.MintTokens(suite.T(), transfer, false)
+	e2e.WaitForEvent(suite.T(), received1, core.EventTypeTransferConfirmed, transferOut.LocalID)
+	e2e.ValidateAccountBalances(suite.T(), client1, poolID, "", map[string]int64{
+		suite.key: 1,
+	})
+
+	transfer = &core.TokenTransferInput{
+		TokenTransfer: core.TokenTransfer{Amount: *fftypes.NewFFBigInt(1), Key: suite.key},
+		Pool:          poolName,
+	}
+	transferOut = client1.BurnTokens(suite.T(), transfer, false)
+	e2e.WaitForEvent(suite.T(), received1, core.EventTypeTransferConfirmed, transferOut.LocalID)
+	e2e.ValidateAccountBalances(suite.T(), client1, poolID, "", map[string]int64{
+		suite.key: 0,
+	})
+}
