6846cb5485111afa98d17952869b5c4d53475f1c
Andrew Richardson <andrew.richardson@kaleido.io>
2022-08-03 15:44:55 -0400

    Remove descriptions for non-existent namespace fields
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- internal/coremsgs/en_struct_descriptions.go
+++ internal/coremsgs/en_struct_descriptions.go
@@ -376,12 +376,9 @@ var (
 	VerifierCreated   = ffm("Verifier.created", "The time this verifier was created on this node")
 
 	// Namespace field descriptions
-	NamespaceID                    = ffm("Namespace.id", "The UUID of the namespace. For locally established namespaces will be different on each node in the network. For broadcast namespaces, will be the same on every node")
-	NamespaceMessage               = ffm("Namespace.message", "The UUID of broadcast message used to establish the namespace. Unset for local namespaces")
 	NamespaceName                  = ffm("Namespace.name", "The local namespace name")
 	NamespaceRemoteName            = ffm("Namespace.remoteName", "The namespace name within the multiparty network")
 	NamespaceDescription           = ffm("Namespace.description", "A description of the namespace")
-	NamespaceType                  = ffm("Namespace.type", "The type of the namespace")
 	NamespaceCreated               = ffm("Namespace.created", "The time the namespace was created")
 	MultipartyContractsActive      = ffm("MultipartyContracts.active", "The currently active FireFly smart contract")
 	MultipartyContractsTerminated  = ffm("MultipartyContracts.terminated", "Previously-terminated FireFly smart contracts")
