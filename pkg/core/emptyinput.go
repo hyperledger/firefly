6ebba72f79f14eb8ebad3a57c84964c1a8cbc265
Peter Broadhurst <peter.broadhurst@kaleido.io>
2022-09-01 13:03:30 -0400

    Add routes to directly publish blob/JSON data to shared storage (IFPS etc.)
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- pkg/core/emptyinput.go
+++ pkg/core/emptyinput.go
@@ -17,4 +17,4 @@
 package core
 
 // EmptyInput represents an API with no input fields, but requiring a JSON content type on the request
-type EmptyInput struct{}
+type EmptyInput map[string]interface{}
