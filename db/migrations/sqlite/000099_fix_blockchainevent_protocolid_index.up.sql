d75f2d4a0cd689df174d57d8e40d4c623c129e0d
Andrew Richardson <andrew.richardson@kaleido.io>
2022-09-07 10:16:45 -0400

    Properly enforce uniqueness of BatchPin blockchain events
    Because listener_id is NULL for these events, we need separate indexes for
    when it is NULL and not NULL.
    
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

---
+++ db/migrations/sqlite/000099_fix_blockchainevent_protocolid_index.up.sql
@@ -0,0 +1,3 @@
+DROP INDEX blockchainevents_protocolid;
+CREATE UNIQUE INDEX blockchainevents_protocolid ON blockchainevents(namespace, protocol_id) WHERE listener_id IS NULL;
+CREATE UNIQUE INDEX blockchainevents_listener_protocolid ON blockchainevents(namespace, listener_id, protocol_id) WHERE listener_id IS NOT NULL;
