b61fb1373eae7ef4bfcddbd3d7fe8f17e92be481
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2024-02-01 18:42:17 -0500

    updating docker_build.sh to ensure exit 1 if anything fails
    Signed-off-by: Samim Mirhosseini <ssmirr@users.noreply.github.com>

--- docker_build.sh
+++ docker_build.sh
@@ -16,9 +16,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
 echo $@
 
- if [[ ! -x `which jq` ]]; then echo "Please install \"jq\" to continue"; exit 1; fi
+if [[ ! -x `which jq` ]]; then echo "Please install \"jq\" to continue"; exit 1; fi
 
 FIREFLY_BUILDER_TAG=$(cat manifest.json | jq -r '.build."firefly-builder".image')
 FABRIC_BUILDER_TAG=$(cat manifest.json | jq -r '.build."fabric-builder".image')
