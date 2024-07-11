fc18b63d30a130c55fae8a8c8a22a757f259d120
Enrique Lacal <enrique.lacal@kaleido.io>
2024-07-11 15:28:33 +0100

    move to VARCHAR for postgres
    Signed-off-by: Enrique Lacal <enrique.lacal@kaleido.io>

--- db/migrations/postgres/000117_update_contractlisteners_add_filters_column.up.sql
+++ db/migrations/postgres/000117_update_contractlisteners_add_filters_column.up.sql
@@ -1,5 +1,5 @@
 BEGIN;
 ALTER TABLE contractlisteners ADD COLUMN filters TEXT;
 -- changing the length of varchar does not affect the index
-ALTER TABLE contractlisteners ALTER COLUMN signature TYPE VARCHAR(MAX); 
+ALTER TABLE contractlisteners ALTER COLUMN signature TYPE VARCHAR; 
 COMMIT;
